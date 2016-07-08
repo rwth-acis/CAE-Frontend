a4f0ef91e91b8ff50825f6d16295264d6a74f7b0
Thomas Winkler <winkler@ingame.de>
2016-07-08 17:47:08 +0200

    add: configuration example

--- widgets/codeEditorWidget/src/lib/config.js
+++
@@ -1,8 +0,0 @@
-export default{
-  GitHubProxyService:{
-    endPointBase : "http://localhost:8080/CAE/github"
-  },
-  CodeEditorWidget:{
-    bower_components : "http://localhost/liveCodeEditorWidget/bower_components/"//'http://eiche.informatik.rwth-aachen.de/editor/codeEditor/codeEditorWidget/bower_components', // location of the y-* modules
-  }
-}
