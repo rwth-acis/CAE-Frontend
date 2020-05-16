a825c698d02183cc878aa90453e33366631fb5fa
Philipp Dolif <git.phil.cd@gmail.com>
2020-05-16 14:16:57 +0200

    Project User Widget: User list and add user button

--- cae-app/src/project-management/project-user.js
+++ cae-app/src/project-management/project-user.js
@@ -1,8 +1,9 @@
 import {html, LitElement} from 'lit-element';
 import '@polymer/paper-card/paper-card';
 import '@polymer/paper-dropdown-menu/paper-dropdown-menu.js';
-import '@polymer/paper-item/paper-item.js'
+import '@polymer/paper-item/paper-item.js';
 import '@polymer/paper-listbox/paper-listbox.js';
+import '@polymer/iron-icon/iron-icon';
 
 /**
  * PolymerElement for management of project users.
@@ -14,6 +15,9 @@ class ProjectUser extends LitElement {
   render() {
     return html`
       <style>
+        :host {
+          font-family: Roboto;
+        }
         .main{
           width: 100%;
           margin-top: 1em;
@@ -22,6 +26,29 @@ class ProjectUser extends LitElement {
         }
         .dropdown-menu {
           width: 100%;
+        }
+        .separator {
+           border-top: thin solid #eeeeee;
+        }
+        .input-username {
+          width: 100%;
+          margin-right: 0.5em;
+          border-radius: 3px;
+          border: thin solid #e6e6e6;
+          height: 2.5em;
+          padding-left:5px;
+        }
+        paper-button {
+          color: rgb(240,248,255);
+          background: rgb(30,144,255);
+          height: 2.5em;
+        }
+        paper-button:hover {
+          color: rgb(240,248,255);
+          background: rgb(65,105,225);
+        }
+        .edit-icon {
+          color: #c6c6c6;
         }
       </style>
       <div class="main">
@@ -34,36 +61,57 @@ class ProjectUser extends LitElement {
           </paper-listbox>
         </paper-dropdown-menu>
         </div>
-        <div class="user-list" style="margin-left: 1em; margin-right: 1em">
-          <ul>
+        <div class="user-list" style="margin-left: 1em; margin-right: 1em; overflow: auto; max-height: 25em">
+          ${this.projectSelected ? html`` : html`<p>No project selected.</p>`}
             ${this.userList.map(user => html`
-              <li>
+              <div style="width: 100%; display: flex; align-items: center">
                 <p>${user.name}</p>
-                <hr>
-              </li>
+                <p style="margin-right: 0.5em; margin-left: auto">${user.role}</p>
+                <iron-icon class="edit-icon" icon="create"></iron-icon>
+              </div>
+              <div class="separator"></div>
             `)}
-          </ul>
         </div>
+        ${this.projectSelected ? html`
+          <div class="add-user" style="display: flex; margin-top: 0.5em; margin-left: 1em; margin-right: 1em">
+            <input class="input-username" placeholder="Enter Username" style="margin-left: 0"></input>
+            <paper-button @click="${this._onAddUserToProjectClicked}" style="margin-left: auto">Add</paper-button>
+          </div>
+        ` : html``}
       </div>
     `;
+  }
+
+  widgetClicked() {
+    console.log('widget clicked');
+    this.$.modal.open();
   }
 
   static get properties() {
     return {
       userList: {
         type: Array
+      },
+      projectSelected: {
+        type: Boolean
       }
     }
   }
 
   constructor() {
     super();
-    this.userList = this.getUsersByProject(1);
+    this.userList = [];
+    this.projectSelected = false;
   }
 
   _onProjectSelected(projectId) {
+    this.projectSelected = true;
     console.log("project with id " + projectId + " got selected");
     this.userList = this.getUsersByProject(projectId);
+  }
+
+  _onAddUserToProjectClicked() {
+    console.log("add user to project clicked");
   }
 
   /**
@@ -131,7 +179,6 @@ class ProjectUser extends LitElement {
       ];
     }
   }
-
 
 }
 
