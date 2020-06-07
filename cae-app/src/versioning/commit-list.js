4542514ca2f75a9f455983aeac69406e99d54698
Philipp Dolif <git.phil.cd@gmail.com>
2020-06-07 19:31:33 +0200

    First version of frontend for commit list

--- cae-app/src/versioning/commit-list.js
+++ cae-app/src/versioning/commit-list.js
@@ -3,8 +3,78 @@ import { LitElement, html} from 'lit-element';
 export class CommitList extends LitElement {
   render() {
     return html`
-      <p>Commit List</p>
+      <style>
+        .separator {
+           border-top: thin solid #e1e1e1;
+        }
+        .label {
+          color: #586069;
+          border: 1px solid #e1e1e1;
+          margin-top: auto;
+          margin-bottom: auto;
+          padding: 0.1em 0.2em;
+          border-radius: 3px;
+        }
+      </style>
+      <h3>Commits</h3>
+      <div class="separator"></div>
+      <div style="overflow: scroll; max-height: 500px">
+        <!-- list commits -->
+        ${this.versionedModel.commits.map(commit => html`
+          <div @click=${() => this._onCommitLeftClicked(commit)} @contextmenu="${(e) => this._onCommitRightClicked(e, commit)}">
+            <div style="display: flex">
+              <!-- version message -->
+              <p style="margin-right: 0.5em">${commit.message}</p>
+              <!-- version tag -->
+              ${commit.tag ? html`<span class="label" style="margin-right: 0.5em">${commit.tag.tag}</span>` : html``}
+            </div>
+            <!-- timestamp -->
+            <p style="color: #aeaeae">${commit.timestamp}</p>
+          </div>
+          <div class="separator"></div>
+        `)}
+      </div>
     `;
+  }
+
+  static get properties() {
+    return {
+      /**
+       * Gets set by versioning-element.
+       */
+      versionedModel: {
+        type: Object
+      }
+    };
+  }
+
+  /**
+   * Gets called when the user left-clicks on a commit in the commit list.
+   * @param commit The commit that was left-clicked.
+   * @private
+   */
+  _onCommitLeftClicked(commit) {
+    console.log("commit left-clicked:");
+    console.log(commit);
+  }
+
+  /**
+   * Gets called when the user right-clicks on a commit in the commit list.
+   * @param event MouseEvent needed to prevent displaying context menu of browser.
+   * @param commit The commit that was right-clicked.
+   * @private
+   */
+  _onCommitRightClicked(event, commit) {
+    // preventDefault ensures that the context menu of the browser does not show up
+    event.preventDefault();
+
+    // notify versioning-element about right click on commit
+    let notifyEvent = new CustomEvent("commit-right-click", {
+      detail: {
+        commit: commit
+      }
+    });
+    this.dispatchEvent(notifyEvent);
   }
 }
 
--- cae-app/src/versioning/versioning-element.js
+++ cae-app/src/versioning/versioning-element.js
@@ -28,13 +28,100 @@ export class VersioningElement extends LitElement {
       
       <div class="container flex-horizontal-with-ratios">
         <div class="flex-commit-list">
-          <commit-list></commit-list>
+          <commit-list versionedModel=${JSON.stringify(this.versionedModel)}
+            @commit-right-click="${(e) => this._onCommitRightClicked(e.detail)}"></commit-list>
         </div>
         <div class="flex-commit-details">
           <commit-details></commit-details>
         </div>
       </div>
     `;
+  }
+
+  static get properties() {
+    return {
+      versionedModel: {
+        type: Object
+      }
+    };
+  }
+
+  constructor() {
+    super();
+    // TODO: this is only some testing data
+    this.versionedModel = {
+      "id": 1,
+      "commits": [
+        {
+          "id": 10,
+          "message": "Another test commit without tag but with a longer text message.",
+          "timestamp": "07.06.2020 18:20"
+        },
+        {
+          "id": 9,
+          "message": "Another test commit without tag but with a longer text message.",
+          "timestamp": "07.06.2020 18:15"
+        },
+        {
+          "id": 8,
+          "message": "Another test commit without tag but with a longer text message.",
+          "timestamp": "07.06.2020 14:46"
+        },
+        {
+          "id": 7,
+          "message": "Another test commit without tag but with a longer text message.",
+          "timestamp": "06.06.2020 09:34"
+        },
+        {
+          "id": 6,
+          "message": "Again a commit with a version tag attached. This one has a longer text message.",
+          "tag": {
+            "id": 2,
+            "tag": "0.1.0"
+          },
+          "timestamp": "05.06.2020 16:20"
+        },
+        {
+          "id": 5,
+          "message": "Another test commit without tag but with a longer text message.",
+          "timestamp": "05.06.2020 13:17"
+        },
+        {
+          "id": 4,
+          "message": "Another test commit without tag but with a longer text message.",
+          "timestamp": "04.06.2020 22:24"
+        },
+        {
+          "id": 3,
+          "message": "Another test commit without tag but with a longer text message.",
+          "timestamp": "04.06.2020 16:10"
+        },
+        {
+          "id": 2,
+          "message": "This commit has a version tag attached.",
+          "tag": {
+            "id": 1,
+            "tag": "0.0.1"
+          },
+          "timestamp": "02.06.2020 11:20"
+        },
+        {
+          "id": 1,
+          "message": "This is a test commit message for Commit 1.",
+          "timestamp": "20.05.2020 13:46"
+        }
+      ]
+    };
+  }
+
+  /**
+   * Gets called by the commit-list when the user right clicks on a commit.
+   * @param eventDetail Details of the event containing the clicked commit.
+   * @private
+   */
+  _onCommitRightClicked(eventDetail) {
+    const commit = eventDetail.commit;
+    console.log(commit);
   }
 }
 
