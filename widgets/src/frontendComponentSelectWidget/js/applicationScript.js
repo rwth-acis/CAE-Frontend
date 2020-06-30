f86fbf02400a2c6eac72c24d58a6b00990e73bba
Philipp Dolif <git.phil.cd@gmail.com>
2020-06-30 11:43:34 +0200

    Updated component-select widgets

--- widgets/src/frontendComponentSelectWidget/js/applicationScript.js
+++ widgets/src/frontendComponentSelectWidget/js/applicationScript.js
@@ -37,7 +37,7 @@
        console.log(intent);
      };
 
-     client = new Las2peerWidgetLibrary("@@caehost/CAE/models", iwcCallback, '*');
+     client = new Las2peerWidgetLibrary("@@caehost/project-management/projects", iwcCallback, '*');
 
      getServices()
  });
@@ -54,46 +54,36 @@
 
 /**
  *
- * Calls the persistence service first for a list of services,
- * then retrieves all services and adds all frontend components
+ * Calls the project-management service first for a list of components,
+ * then retrieves all components and adds all frontend components
  * to the frontend component table.
  *
  */
 var getServices = function() {
-  client.sendRequest("GET", "", "", "application/json", {}, false,
-  function(data, type) {
-      $.each(data, function(index, value) {
-        client.sendRequest("GET", value, "", "application/json", {}, false,
-        function(data, type) {
-          // add table rows
-          var name = data.attributes.label.value.value;
-          var type;
-          var version;
-          $.each(data.attributes.attributes, function(index, value) {
-            if(value.name == "version"){
-              version = value.value.value;
-            }
-            if(value.name == "type"){
-              type = value.value.value;
-            }
-          });
-          if(type == "frontend-component"){
-          $("#frontendComponentTable").append("<tr><td>" + name +
-            "</td><td>" + version + "</td></tr>");
-          // make table rows "clickable"
-          $("#frontendComponentTable").find("tr").click(function() {
-            // get the name
-            var name = $(this).find("td").get(0).innerHTML;
-            createNode(name);
-          });
-          }
-        }, function(error) {
-          console.log(error);
-          $("#frontendComponentTable").html(error);
-        })
+  const modelingInfo = JSON.parse(localStorage.getItem("modelingInfo"));
+  const currentProjectId = modelingInfo.application.projectId;
+
+  client.sendRequest("GET", currentProjectId + "/components", "", "application/json", {}, false, function(data, type) {
+    const projectComponents = JSON.parse(data);
+    const projectFrontendComponents = projectComponents.filter(component => component.type == "frontend");
+
+    $.each(projectFrontendComponents, function(index, value) {
+      // add table rows
+      var name = value.name;
+      var version = "NOT YET IMPLEMENTED";
+
+      $("#frontendComponentTable").append("<tr><td>" + name +
+        "</td><td>" + version + "</td></tr>");
+      // make table rows "clickable"
+      $("#frontendComponentTable").find("tr").click(function() {
+        // get the name
+        var name = $(this).find("td").get(0).innerHTML;
+        createNode(name);
       });
+    });
+
   }, function(error) {
     console.log(error);
     $("#frontendComponentTable").html(error);
-  })
+  });
 };
--- widgets/src/microserviceSelectWidget/js/applicationScript.js
+++ widgets/src/microserviceSelectWidget/js/applicationScript.js
@@ -5,7 +5,7 @@ $(function() {
     var iwcCallback = function(intent) {
       console.log(intent);
     };
-    client = new Las2peerWidgetLibrary("@@caehost/CAE/models", iwcCallback, '*');
+    client = new Las2peerWidgetLibrary("@@caehost/project-management/projects", iwcCallback, '*');
 
     getServices()
 });
@@ -23,47 +23,36 @@ function createNode(name, version) {
 
 /**
  *
- * Calls the persistence service first for a list of services,
- * then retrieves all services and adds all microservices
- * to the microservice table.
+ * Calls the project-management service first for a list of components,
+ * then retrieves all components and adds all frontend components
+ * to the frontend component table.
  *
  */
 var getServices = function() {
-  client.sendRequest("GET", "", "", "application/json", {}, false,
-  function(data, type) {
-      $.each(data, function(index, value) {
-        client.sendRequest("GET", value, "", "application/json", {}, false,
-        function(data, type) {
-          // add table rows
-          var name = data.attributes.label.value.value;
-          var type;
-          var version;
-          $.each(data.attributes.attributes, function(index, value) {
-            if(value.name == "version"){
-              version = value.value.value;
-            }
-            if(value.name == "type"){
-              type = value.value.value;
-            }
-          });
-          if(type == "microservice"){
-          $("#microserviceTable").append("<tr><td>" + name +
-            "</td><td>" + version + "</td></tr>");
-          // make table rows "clickable"
-          $("#microserviceTable").find("tr").click(function() {
-            // get the name
-            var name = $(this).find("td").get(0).innerHTML;
-            var version = $(this).find("td").get(1).innerHTML;
-            createNode(name, version);
-          });
-          }
-        }, function(error) {
-          console.log(error);
-          $("#microserviceTable").html(error);
-        })
+  const modelingInfo = JSON.parse(localStorage.getItem("modelingInfo"));
+  const currentProjectId = modelingInfo.application.projectId;
+
+  client.sendRequest("GET", currentProjectId + "/components", "", "application/json", {}, false, function(data, type) {
+    const projectComponents = JSON.parse(data);
+    const projectMicroservices = projectComponents.filter(component => component.type == "microservice");
+
+    $.each(projectMicroservices, function(index, value) {
+      // add table rows
+      var name = value.name;
+      var version = "NOT YET IMPLEMENTED";
+
+      $("#microserviceTable").append("<tr><td>" + name +
+        "</td><td>" + version + "</td></tr>");
+      // make table rows "clickable"
+      $("#microserviceTable").find("tr").click(function() {
+        // get the name
+        var name = $(this).find("td").get(0).innerHTML;
+        createNode(name);
       });
+    });
+
   }, function(error) {
     console.log(error);
     $("#microserviceTable").html(error);
-  })
+  });
 };
