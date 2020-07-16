632b2d6493924eea8759bf0484c40dfc40b67562
Philipp Dolif <git.phil.cd@gmail.com>
2020-07-16 14:06:57 +0200

    Display correct component name in Swagger Editor

--- widgets/src/swaggerWidget/js/swaggerUiEditor.js
+++ widgets/src/swaggerWidget/js/swaggerUiEditor.js
@@ -143,7 +143,11 @@ var loadMetadata = function(y) {
         loadedModel = currentComponentName;
     } else if (y.share.data.get('model')) {
         var data = y.share.data.get('model');
-        loadedModel = localStorage.getItem("versionedModelId");
+
+        var modelingInfo = JSON.parse(localStorage.getItem("modelingInfo"));
+        var componentName = modelingInfo.microservice.name;
+
+        loadedModel = componentName;
         // special case if model was only saved in the space (not loaded from db)
         if (loadedModel.toUpperCase() == "Model attributes".toUpperCase()) {
             loadedModel = null;
