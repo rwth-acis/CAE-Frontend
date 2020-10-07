797a2de3acfc0751184a3705502a28cf4ea245fb
Philipp Dolif <git.phil.cd@gmail.com>
2020-10-07 12:04:45 +0200

    Fixed #34 (incorrect timestamp in commit list)

--- cae-app/src/versioning/commit-list.js
+++ cae-app/src/versioning/commit-list.js
@@ -235,7 +235,7 @@ export class CommitList extends LitElement {
     const date = new Date(originalTimestamp + "Z");
 
     const year = date.getFullYear();
-    const month = date.getMonth() < 10 ? "0" + (date.getMonth()+1) : (date.getMonth()+1);
+    const month = date.getMonth()+1 < 10 ? "0" + (date.getMonth()+1) : (date.getMonth()+1);
     const day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
     const hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
     const minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
