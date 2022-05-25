62d902e52dd52662738e7d36971fc2f61c914a56
Philipp Dolif <git.phil.cd@gmail.com>
2022-05-25 12:58:53 +0200

    Request difference calculation #76

--- cae-app/src/util/model-differencing/test-model-differencing.js
+++ cae-app/src/util/model-differencing/test-model-differencing.js
@@ -3,6 +3,10 @@ import TestCaseAddition from "./test/test-case-addition";
 import TestCaseDeletion from "./test/test-case-deletion";
 import TestCaseUpdate from "./test/test-case-update";
 import TestCaseDifference from "./test/test-case-difference";
+import RequestDifference from "./test/request-difference";
+import RequestAddition from "./test/request-addition";
+import RequestDeletion from "./test/request-deletion";
+import RequestUpdate from "./test/request-update";
 
 /**
  * Class used for calculating the differences between two versions of a test model.
@@ -19,9 +23,15 @@ export default class TestModelDifferencing {
   static getDifferences(model1TestCases, model2TestCases) {
     let differences = [];
 
+    // test case changes
     differences = differences.concat(TestModelDifferencing.getTestCaseAdditions(model1TestCases, model2TestCases));
     differences = differences.concat(TestModelDifferencing.getTestCaseDeletions(model1TestCases, model2TestCases));
     differences = differences.concat(TestModelDifferencing.getUpdatedTestCases(model1TestCases, model2TestCases));
+
+    // request changes
+    differences = differences.concat(TestModelDifferencing.getRequestAdditions(model1TestCases, model2TestCases));
+    differences = differences.concat(TestModelDifferencing.getRequestDeletions(model1TestCases, model2TestCases));
+    differences = differences.concat(TestModelDifferencing.getUpdatedRequests(model1TestCases, model2TestCases));
 
     return differences;
   }
@@ -72,6 +82,34 @@ export default class TestModelDifferencing {
   }
 
   /**
+   * Searches for requests that got added to a test case.
+   * @param {*} model1TestCases Previous state of the test case list.
+   * @param {*} model2TestCases Current state of the test case list.
+   * @returns List of RequestAddition objects.
+   */
+  static getRequestAdditions(model1TestCases, model2TestCases) {
+    const testCaseAdditions = TestModelDifferencing.getTestCaseAdditions(model1TestCases, model2TestCases);
+    const requestAdditions = [];
+
+    for(let testCase of model2TestCases) {
+      // check if test case was added => then we do not take the requests into account
+      const testCaseAddition = testCaseAdditions.find(t => t.updatedTestCase.id == testCase.id);
+      if(!testCaseAddition) {
+        // test case was not added, so check if its requests were updated
+        const oldTestCase = model1TestCases.find(t => t.id == testCase.id);
+        for(let request of testCase.requests) {
+          if(!oldTestCase.requests.find(r => r.id == request.id)) {
+            // this request was added
+            requestAdditions.push(new RequestAddition(request, testCase));
+          }
+        }
+      }
+    }
+
+    return requestAdditions;
+  }
+
+  /**
    * Searches for test cases that got removed from the test model.
    * @param {*} model1TestCases Previous state of the test case list.
    * @param {*} model2TestCases Current state of the test case list.
@@ -83,6 +121,32 @@ export default class TestModelDifferencing {
       // check if test case got removed in model2
       if(!model2TestCases.find(testCase2 => testCase2.id == testCase.id)) {
         deletions.push(new TestCaseDeletion(testCase));
+      }
+    }
+    return deletions;
+  }
+
+  /**
+   * Searches for requests that got removed from a test case.
+   * @param {*} model1TestCases Previous state of the test case list.
+   * @param {*} model2TestCases Current state of the test case list.
+   * @returns List of RequestDeletion objects.
+   */
+  static getRequestDeletions(model1TestCases, model2TestCases) {
+    // deleted test cases do not need to be taken into account
+    const deletions = [];
+    for(let testCase of model2TestCases) {
+      // check if test case already existed in model1
+      const oldTestCase = model1TestCases.find(t => t.id == testCase.id);
+      if(oldTestCase) {
+        // test case already existed in model1
+        // check if a request got removed
+        for(let request of oldTestCase.requests) {
+          if(!testCase.requests.find(r => r.id == request.id)) {
+            // this request got removed
+            deletions.push(new RequestDeletion(request, testCase));
+          }
+        }
       }
     }
     return deletions;
@@ -114,6 +178,38 @@ export default class TestModelDifferencing {
   }
 
   /**
+   * Searches for requests that are included in both lists but got updated.
+   * @param {*} model1TestCases Previous state of the test case list.
+   * @param {*} model2TestCases Current state of the test case list.
+   * @returns List of RequestUpdate objects.
+   */
+  static getUpdatedRequests(model1TestCases, model2TestCases) {
+    const updatedRequests = [];
+
+    // check for test cases which are part of both lists
+    for(let testCase1 of model1TestCases) {
+      const testCase2 = model2TestCases.find(testCase => testCase.id == testCase1.id);
+      if(testCase2) {
+        // test case is available in both lists
+        // check for requests which are part of both test cases
+        for(let request1 of testCase1.requests) {
+          const request2 = testCase2.requests.find(r => r.id == request1.id);
+          if(request2) {
+            // request is available in both test cases
+            // check if request has changed
+            const updatedKeys = TestModelDifferencing.getRequestUpdatedKeys(request1, request2);
+            if(updatedKeys.length > 0) {
+              updatedRequests.push(new RequestUpdate(request1, request2, testCase2, TestModelDifferencing.getRequestRelevantKeys(request1), updatedKeys));
+            }
+          }
+        }
+      }
+    }
+
+    return updatedRequests;
+  }
+
+  /**
    * Searches for keys in the given test case objects, where the corresponding value changed.
    * @param {*} testCase1 Previous state of the test case.
    * @param {*} testCase2 Current state of the test case.
@@ -131,12 +227,48 @@ export default class TestModelDifferencing {
   }
 
   /**
+   * Searches for keys in the given request objects, where the corresponding value changed.
+   * @param {*} request1 Previous state of the request.
+   * @param {*} request2 Current state of the request.
+   * @returns 
+   */
+  static getRequestUpdatedKeys(request1, request2) {
+    let updatedKeys = [];
+    for(let key of TestModelDifferencing.getRequestRelevantKeys(request1)) {
+      if(key == "auth") {
+        if(!Object.keys(request1.auth).includes("selectedAgent") && !Object.keys(request2.auth).includes("selectedAgent")) {
+          // both requests have auth disabled
+        } else if(Object.keys(request1.auth).includes("selectedAgent") && Object.keys(request2.auth).includes("selectedAgent")) {
+          // both requests have auth enabled
+          if(request1.auth.selectedAgent != request2.auth.selectedAgent) {
+            updatedKeys.push(key);
+          }
+        } else {
+          updatedKeys.push(key);
+        }
+      } else if(request1[key] != request2[key]) {
+        updatedKeys.push(key);
+      }
+    }
+    return updatedKeys;
+  }
+
+  /**
    * Filters the keys of the given test case object for the keys that are relevant for calculating the differences.
    * @param {*} testCase 
    * @returns Keys that are relevant for calculating test case differences.
    */
   static getTestCaseRelevantKeys(testCase) {
     return Object.keys(testCase).filter(key => !["id", "status", "requests"].includes(key));
+  }
+
+  /**
+   * Filters the keys of the given request object for the keys that are relevant for calculating the differences.
+   * @param {*} request 
+   * @returns Keys that are relevant for calculating request differences.
+   */
+  static getRequestRelevantKeys(request) {
+    return Object.keys(request).filter(key => !["id", "status", "assertions"].includes(key));
   }
 
   /**
@@ -159,5 +291,17 @@ export default class TestModelDifferencing {
         return diff1.getTestCaseId() == diff2.getTestCaseId();
       }
     }
+    if(diff1 instanceof RequestDifference && diff2 instanceof RequestDifference) {
+      if(diff1 instanceof RequestAddition && diff2 instanceof RequestAddition) {
+        return diff1.getRequestId() == diff2.getRequestId();
+      }
+      if(diff1 instanceof RequestDeletion && diff2 instanceof RequestDeletion) {
+        return diff1.getRequestId() == diff2.getRequestId();
+      }
+      if(diff1 instanceof RequestUpdate && diff2 instanceof RequestUpdate) {
+        return diff1.getRequestId() == diff2.getRequestId();
+      }
+    }
+    return false;
   }
 }
